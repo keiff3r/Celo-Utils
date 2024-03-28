3d6cc34df068caa4efed60224788819d4ffcaab6
keiff3r <keiffer@blooo.io>
2024-03-28 20:43:54 +0100

    Update generateFeeTokenInfo.js to convert feeTokenInfo.data to a hex string

--- generateFeeTokenInfo.js
+++ generateFeeTokenInfo.js
@@ -2,10 +2,17 @@ const { RLPEncodedTx, Signer } = require('@celo/connect')
 const { tokenInfoByAddressAndChainId } = require('@celo/wallet-ledger/lib/tokens')
 
 const feeCurrency = '0x765de816845861e75a25fca122bb6898b8b1282a'
-const chainId = 42220
+const TEST_NET = 44787
+const MAIN_NET = 42220
+const chainId = MAIN_NET
 
 const feeTokenInfo = tokenInfoByAddressAndChainId(
     feeCurrency,
     chainId
   )
-    console.log(feeTokenInfo.data)
+    data = (feeTokenInfo.data)
+    // chango to str
+    data = data.toString('hex')
+    console.log(feeTokenInfo)
+    console.log('data: ', data)
+    console.log('data length:', data.length)
